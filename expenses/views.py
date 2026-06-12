3bfbbc94ee5c2a70f66335302ecbec9580eafd95
suyog khadka <ksuyog697@gmail.com>
2026-06-12 23:43:27 +0545

    merge conflict solve

--- expenses/views.py
+++ expenses/views.py
@@ -1,71 +1,44 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
-
+from django.db.models import Sum
 from .models import Category, Expense
 from .serializers import CategorySerializer, ExpenseSerializer
 
 
-@api_view(["GET", "POST"])
-def category_list(request):
-    if request.method == "GET":
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data)
-
-    serializer = CategorySerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
 
-@api_view(["GET", "POST"])
-def expense_list(request):
-    if request.method == "GET":
-        expenses = Expense.objects.all()
+class ExpenseViewSet(viewsets.ModelViewSet):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
 
-        start_date = request.query_params.get("start_date")
-        end_date = request.query_params.get("end_date")
+    def get_queryset(self):
+        """
+        Filter expenses by date range if provided in query parameters.
+        """
+        queryset = super().get_queryset()
+        
+        start_date = self.request.query_params.get("start_date")
+        end_date = self.request.query_params.get("end_date")
         if start_date:
-            expenses = expenses.filter(date__gte=start_date)
+            queryset = queryset.filter(date__gte=start_date)
         if end_date:
-            expenses = expenses.filter(date__lte=end_date)
+            queryset = queryset.filter(date__lte=end_date)
+        return queryset
 
-        serializer = ExpenseSerializer(expenses, many=True)
-        return Response(serializer.data)
-
-    serializer = ExpenseSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serialzer.data, status=status.HTTP_201_CREATED)
-
-
-@api_view(["GET", "PUT", "DELETE"])
-def expense_detail(request, pk):
-    try:
-        expense = Expense.objects.get(pk=pk)
-    except Expense.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == "GET":
-        serializer = ExpenseSerializer(expense)
-        return Response(serializer.data)
-
-    if request.method == "PUT":
-        serializer = ExpenseSerializer(expense, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    expense.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(["GET"])
-def expense_summary(request):
-    summary = (
-        Expense.objects.values("category__name")
-        .annotate(total=Sum("amount"))
-        .order_by("category__name")
-    )
-    return Response(list(summary))
+    @action(detail=False, methods=["get"], url_path="summary")
+    def expense_summary(self, request):
+        """
+        Custom action to get total spent per category.
+        GET /api/expenses/summary/
+        """
+        summary = (
+            Expense.objects
+            .values("category__name")
+            .annotate(total=Sum("amount"))
+            .order_by("category__name")
+        )
+        return Response(list(summary))
